0c7bea0127f686338373f4061863dfc13c617a0e
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-12 21:26:21 +0900

    add toggle route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    member do
+      patch :toggle
+    end
+  end
   resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

0d4a63bc71c9a20a4238882f590c5f63ce8d1fbc
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-15 20:35:14 +0900

    delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
     end
   end
   resources :todos
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end

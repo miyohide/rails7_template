5eff504e663bc8521e7a8df1435e35e8fa3b7959
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-26 21:07:47 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :tasks do
-    member do
-      patch :toggle
-    end
-  end
-  resources :todos
-  resources :quotes
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end

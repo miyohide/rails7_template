b8a43055ff94086150a468e7ed301aa663d0e333
Hidenori Miyoshi <miyohide@gmail.com>
2022-04-29 20:59:10 +0900

    use headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # ブラウザを開いてテストを実行する
+  # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # headless chromeを使ってテストを実行する
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 end

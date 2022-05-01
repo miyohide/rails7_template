d865ec948b3e80118d2163a453960c0f9ccdc036
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-01 16:05:32 +0900

    update system test

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,23 +5,25 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first)
   end
 
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
   test "Creating a new quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
 
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
-  end
-
-  test "Showing a quote" do
-    visit quotes_path
-    click_link @quote.name
   end
 
   test "Updating a quote" do
@@ -29,9 +31,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"

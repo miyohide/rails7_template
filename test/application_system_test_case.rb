0370906525a3714b9a17744a7003862e9486bb7e
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-09 20:48:40 +0900

    include Warden Test Helper

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Warden::Test::Helpers
   # ブラウザを開いてテストを実行する
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
   # headless chromeを使ってテストを実行する

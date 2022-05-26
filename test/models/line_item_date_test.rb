ca3dcd2049f3df3f471c0a0a107ad76eb006e599
Hidenori Miyoshi <miyohide@gmail.com>
2022-05-26 20:19:58 +0900

    update test description

--- test/models/line_item_date_test.rb
+++ test/models/line_item_date_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 class LineItemDateTest < ActiveSupport::TestCase
-  test "#previous_date returns the quote's previous date when it exists" do
+  test "#previous_date はquoteが存在しているとき、その前の日付のものを返す" do
     assert_equal line_item_dates(:today), line_item_dates(:next_week).previous_date
   end
 
-  test "#previous_date returns nil when the quote has no previous date" do
+  test "#previous_date はquoteが存在していなかったとき、nilを返す" do
     assert_nil line_item_dates(:today).previous_date
   end
 end

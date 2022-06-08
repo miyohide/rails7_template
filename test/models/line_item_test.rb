aed6767dbd26e192ec55c144c7d4916a0460b6f2
Hidenori Miyoshi <miyohide@gmail.com>
2022-06-08 20:41:01 +0900

    add line_item#total_price test

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#total_price は1行のアイテムの合計値段を返す" do
+    assert_equal 250, line_items(:catering_today).total_price
+  end
 end
